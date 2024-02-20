1fd044a74eff3457ff3427680e1d89a48b471dae
Yudha Haris Purnama <yudha@clade.ventures>
2024-02-20 19:18:03 +0700

    feat(product): implement app bar

--- lib/features/product/presentation/home/pages/product_home_page.dart
+++ lib/features/product/presentation/home/pages/product_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:boilerplate/features/product/presentation/home/widgets/product_home_app_bar_widget.dart';
 import 'package:flutter/material.dart';
 
 class ProductHomePage extends StatefulWidget {
@@ -10,13 +11,45 @@ class ProductHomePage extends StatefulWidget {
 }
 
 class _ProductHomePageState extends State<ProductHomePage> {
+  late TextEditingController _searchController;
+
+  @override
+  void initState() {
+    super.initState();
+    _searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _searchController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        color: Colors.white,
-        child: const Text('Product Home'),
-      ),
+    return CustomScrollView(
+      slivers: <Widget>[
+        ProductHomeAppBarWidget(
+          controller: _searchController,
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (_, int index) {
+              return ListTile(
+                leading: Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  child: const Placeholder(),
+                ),
+                title: Text(
+                  'Place ${index + 1}',
+                ),
+              );
+            },
+            childCount: 20,
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/features/product/presentation/home/widgets/product_home_app_bar_widget.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../design/constants/colors.dart';
+import '../../../../../design/widgets/app_text_field.dart';
+
+class ProductHomeAppBarWidget extends StatelessWidget {
+  final TextEditingController controller;
+
+  const ProductHomeAppBarWidget({
+    super.key,
+    required this.controller,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 256,
+      floating: true,
+      pinned: true,
+      snap: false,
+      backgroundColor: AppColors.primary,
+      flexibleSpace: FlexibleSpaceBar(
+        background: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            bottomLeft: Radius.circular(24.0),
+          ),
+          child: Image.network(
+            'https://cdn.dummyjson.com/product-images/1/thumbnail.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+        ),
+      ),
+      shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(bottomLeft: Radius.circular(24))),
+      bottom: AppBar(
+        forceMaterialTransparency: true,
+        toolbarHeight: 88,
+        title: Container(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 24,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(
+              24,
+            ),
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                child: AppTextField(
+                  controller: controller,
+                  hint: 'Masukkan pencarian...',
+                ),
+              ),
+              const Icon(
+                Icons.search,
+                color: AppColors.primary,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
