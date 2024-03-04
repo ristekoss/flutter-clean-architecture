2ba28628fd8a83d42e35ffdc6b61cedc4112d3ca
Yudha Haris Purnama <yudha@clade.ventures>
2024-03-04 09:13:17 +0700

    feat(project): update module_generator.dart to case safety

--- codegen/module_generator.dart
+++ codegen/module_generator.dart
@@ -3,44 +3,45 @@ import 'dart:io';
 void main() async {
   print("Enter module name: ... (example: profile) ");
   // Reading name of the Geek
-  String module = await stdin.readLineSync() ?? "";
+  String input = await stdin.readLineSync() ?? "";
+  String module = input.toLowerCase();
 
   // Define the list of folders and files
   List<String> folders = [
-    'lib/features/${module}/data/local',
-    'lib/features/${module}/data/model/mapper',
-    'lib/features/${module}/data/model/responses',
-    'lib/features/${module}/data/remote',
-    'lib/features/${module}/di',
-    'lib/features/${module}/domain',
-    'lib/features/${module}/domain/model',
-    'lib/features/${module}/domain/repository',
-    'lib/features/${module}/domain/use_cases',
-    'lib/features/${module}/presentation/blocs/events',
-    'lib/features/${module}/presentation/blocs/states',
-    'lib/features/${module}/presentation/blocs',
-    'lib/features/${module}/presentation/pages',
-    'lib/features/${module}/presentation/widgets',
+    'lib/features/$module/data/local',
+    'lib/features/$module/data/model/mapper',
+    'lib/features/$module/data/model/responses',
+    'lib/features/$module/data/remote',
+    'lib/features/$module/di',
+    'lib/features/$module/domain',
+    'lib/features/$module/domain/model',
+    'lib/features/$module/domain/repository',
+    'lib/features/$module/domain/use_cases',
+    'lib/features/$module/presentation/blocs/events',
+    'lib/features/$module/presentation/blocs/states',
+    'lib/features/$module/presentation/blocs',
+    'lib/features/$module/presentation/pages',
+    'lib/features/$module/presentation/widgets',
   ];
 
   List<String> files = [
-    'lib/features/${module}/data/local/${module}_local_data_sources.dart',
-    'lib/features/${module}/data/model/mapper/${module}_mapper.dart',
-    'lib/features/${module}/data/model/responses/${module}_response.dart',
-    'lib/features/${module}/data/remote/${module}_remote_data_sources.dart',
-    'lib/features/${module}/data/${module}_repository_impl.dart',
-    'lib/features/${module}/di/${module}_module.dart',
-    'lib/features/${module}/domain/model/${module}.dart',
-    'lib/features/${module}/domain/repository/${module}_repository.dart',
-    'lib/features/${module}/domain/use_cases/${module}_use_cases.dart',
-    'lib/features/${module}/domain/${module}_interactor.dart',
-    'lib/features/${module}/presentation/blocs/events/get_${module}_event.dart',
-    'lib/features/${module}/presentation/blocs/states/get_${module}_states.dart',
-    'lib/features/${module}/presentation/blocs/${module}_bloc.dart',
-    'lib/features/${module}/presentation/blocs/${module}_events.dart',
-    'lib/features/${module}/presentation/blocs/${module}_states.dart',
-    'lib/features/${module}/presentation/pages/${module}_page.dart',
-    'lib/features/${module}/presentation/widgets/${module}_info_widget.dart',
+    'lib/features/$module/data/local/${module}_local_data_sources.dart',
+    'lib/features/$module/data/model/mapper/${module}_mapper.dart',
+    'lib/features/$module/data/model/responses/${module}_response.dart',
+    'lib/features/$module/data/remote/${module}_remote_data_sources.dart',
+    'lib/features/$module/data/${module}_repository_impl.dart',
+    'lib/features/$module/di/${module}_module.dart',
+    'lib/features/$module/domain/model/$module.dart',
+    'lib/features/$module/domain/repository/${module}_repository.dart',
+    'lib/features/$module/domain/use_cases/${module}_use_cases.dart',
+    'lib/features/$module/domain/${module}_interactor.dart',
+    'lib/features/$module/presentation/blocs/events/get_${module}_event.dart',
+    'lib/features/$module/presentation/blocs/states/get_${module}_states.dart',
+    'lib/features/$module/presentation/blocs/${module}_bloc.dart',
+    'lib/features/$module/presentation/blocs/${module}_events.dart',
+    'lib/features/$module/presentation/blocs/${module}_states.dart',
+    'lib/features/$module/presentation/pages/${module}_page.dart',
+    'lib/features/$module/presentation/widgets/${module}_info_widget.dart',
   ];
 
   // Create folders
@@ -226,7 +227,7 @@ import '/features/${module.toLowerCase()}/domain/use_cases/${module.toLowerCase(
 import '/features/${module.toLowerCase()}/presentation/blocs/${module.toLowerCase()}_bloc.dart';
 import 'package:get_it/get_it.dart';
 
-void register${module}(GetIt di) {
+void register$module(GetIt di) {
   di.registerFactory<${module}RemoteDataSources>(
       () => ${module}RemoteDataSourceImpl(di<NetworkService>()));
   di.registerFactory<${module}LocalDataSources>(
@@ -254,11 +255,11 @@ abstract class ${module}Repository {
 
 String writeModel(String module) {
   return '''
-class ${module} {
+class $module {
   final int id;
   final String username;
 
-  const ${module}({
+  const $module({
     required this.id,
     required this.username,
   });
